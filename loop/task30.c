31249f4568e9efbf9b4b02deb83d26cc03ad03ef
Unmentionable Arletha <unmentia@ti-l3.skd.21-school.uz>
2025-03-09 08:55:05 +0500

    update

--- loop/task30.c
+++ loop/task30.c
@@ -1,7 +1,26 @@
 #include "../function.h"
 
 void loop_30(){
-	printf("\n%s*Romb qolibi*\n\n%sMisol sharti:\n%sn soni berilgan. Geometrik shaklni ekranga chiqaring. 2*n buyerda qatorlar soni.\n\n%sNamuna:\n%sInput: %s5\n%sOutput:\n%s*********\n**** ****\n***   ***\n**     **\n*       *\n*       *\n**     **\n***   ***\n**** ****\n*********\n\n%sSinab ko'rish!\n\n", yellow, green, white, green, violet, white, violet, white, green);
+	printf("\n%s*Romb qolibi*\n\n%sMisol sharti:\n%sn soni berilgan. Geometrik shaklni ekranga chiqaring. 2*n buyerda qatorlar soni.\n\n%sNamuna:\n%sInput: %s5\n%sOutput:\n%s**** ****\n***   ***\n**     **\n*       *\n         \n*       *\n**     **\n***   ***\n**** ****\n\n%sSinab ko'rish!\n\n", yellow, green, white, green, violet, white, violet, white, green);
 
-	printf("\n%sHozircha mavjud emas!\n", red);
+	int a;
+	printf("%sInput: ", violet);
+    scanf("%d", &a);
+	printf("Output:\n");
+    int t = 0;
+    for(int y=0; y<a*2-1; y++){
+        for(int x=0; x<a*2-1; x++){
+            if(x>=a-1-t && x<=a-1+t){
+                printf(" ");
+            } else {
+                printf("*");
+            }
+        } 
+        if(y>=a-1){
+            t--;
+        } else {
+            t++;
+        }
+        printf("\n");
+    }
 }
