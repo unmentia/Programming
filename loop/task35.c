30f3bd2bb33a82d97ad7e65fce3195753174ee9b
Unmentionable Arletha <unmentia@ti-l3.skd.21-school.uz>
2025-03-09 08:19:08 +0500

    update

--- loop/task35.c
+++ loop/task35.c
@@ -1,7 +1,20 @@
 #include "../function.h"
 
 void loop_35(){
-	printf("\n%s*x ning daraja ketma-ketligi*\n\n%sMisol sharti:\n%sx, n sonlari berilgan: x^0 + x^1 + x^2 + x^3 +... x^n. n buyerda ketma-ketlikni n chi hadi. Masalan: [2^0 + 2^1 + 2^3 + 2^4 + 2^5 + 2^6 + 2^7].\n\n%sNamuna:\n%sInput: %sn=7, x=2\n%sOutput:%s255\n\n%sSinab ko'rish!\n\n", yellow, green, white, green, violet, white, violet, white, green);
+	printf("\n%s*x ning daraja ketma-ketligi*\n\n%sMisol sharti:\n%sx, n sonlari berilgan: x^0 + x^1 + x^2 + x^3 +... x^n. n buyerda ketma-ketlikni n chi hadi. Masalan: [2^0 + 2^1 + 2^3 + 2^4 + 2^5 + 2^6 + 2^7].\n\n%sNamuna:\n%sInput: %sx=2, n=7\n%sOutput:%s255\n\n%sSinab ko'rish!\n\n", yellow, green, white, green, violet, white, violet, white, green);
 
-	printf("\n%sHozircha mavjud emas!\n", red);
+	int a, b;
+	printf("%sInput: ", violet);
+	scanf("%d %d", &a, &b);
+	printf("Output: ");
+	int answer = 1;
+	int res = 0;
+	for(int i=0; i<=b; i++){
+		answer = 1;
+		for(int j=0; j<i; j++){
+			answer *= a;
+		}
+		res += answer;
+	}
+	printf("%d\n", res);
 }
